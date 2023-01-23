1a8da0c3ecec08ae8bd8d7f4e4ee8edac2a7556e
Harry Darwin <harryguoien@gmail.com>
2023-01-23 09:57:06 -0500

    adding firebase functions

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 import {
   GoogleAuthProvider,
   getAuth,
