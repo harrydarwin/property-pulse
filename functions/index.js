fa16557c4250616a069aa6fb775367123a834287
Harry Darwin <harryguoien@gmail.com>
2023-02-07 16:25:40 -0500

    Merge branch 'staging' into demo-pages

--- functions/index.js
+++ functions/index.js
@@ -9,34 +9,34 @@ const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(SENDGRID_API_KEY);
 
 exports.firestoreEmail = functions.firestore
-    .document("propertyPulseUsersDb47/{userId}")
-    .onWrite((change, context) => {
-      const userId = context.params.userId;
+  .document("propertyPulseUsersDb47/{userId}")
+  .onWrite((change, context) => {
+    const userId = context.params.userId;
 
-      const db = admin.firestore();
+    const db = admin.firestore();
 
-      return db.collection("propertyPulseUsersDb47").doc(userId)
-          .get()
-          .then((doc)=> {
-            const user = doc.data();
+    return db.collection("propertyPulseUsersDb47").doc(userId)
+      .get()
+      .then((doc) => {
+        const user = doc.data();
 
-            const msg = {
-              to: user.email,
-              from: "hellopropertypulse@gmail.com",
-              subject: "Welcome, " + user.name,
+        const msg = {
+          to: user.email,
+          from: "hellopropertypulse@gmail.com",
+          subject: "Welcome, " + user.name,
 
-              // custom template
-              templateId: "d-afa8f01ffd4544c2b1682080d859b96e",
-              substitutions: {
-                name: user.name,
-              },
-            };
+          // custom template
+          templateId: "d-afa8f01ffd4544c2b1682080d859b96e",
+          substitutions: {
+            name: user.name,
+          },
+        };
 
-            return sgMail.send(msg);
-          })
-          .then(()=> console.log("Email sent!"))
-          .catch((err) => console.log(err));
-    });
+        return sgMail.send(msg);
+      })
+      .then(() => console.log("Email sent!"))
+      .catch((err) => console.log(err));
+  });
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
